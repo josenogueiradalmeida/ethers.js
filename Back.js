ef6bd92b0b247827855e585b9aeebe4a6f38ecae
José Nogueira <josej@bndes.gov.br>
2019-11-13 16:52:37 -0300

    ETH request and receive

--- Back.js
+++ Back.js
@@ -1,47 +1,98 @@
 const ethers = require('ethers');
 const express = require('express');
+const sha3 = require('sha3');
 
 const app = express();
 const port = 3000;
 
-app.get('/requestEth/:id',function( _req , _res ){
-    const id = _req.params.id;
-    console.log('/requestEth/' + id )
-    const result = testParam(id);
+const privateKey = "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d";
+const wallet     = new ethers.Wallet(privateKey);
+const valueInETH = "0.001";
+const gasLimit   = "53000";
+//const provider   = ethers.getDefaultProvider('rinkeby');    
+const provider   = new ethers.providers.JsonRpcProvider();// Default: http://localhost:8545
 
-    _res.send("leu " + id + " | testParam " + result);    
+app.get('/requestEth/:addressto',function( _req , _res ){
+    const addressto = _req.params.addressto;
+    console.log('/requestEth/' + addressto )
+    const isValidAddress = testParam(addressto);
+    
+    if ( isValidAddress ) {
+        requestETH( addressto );
+    } else {
+        console.log("Can not send ETH to an invalid address!")
+    }        
+
+    _res.send("The address: " + addressto + " | is valid? " + isValidAddress);    
     _res.end();    
 });
 
-function testParam( _param ) {
-    return isAddress(_param);
+function requestETH( _addressto ) {    
+    var gasPricePromise         = provider.getGasPrice();
+    var balancePromise          = provider.getBalance(wallet.address);    
+    var transactionCountPromise = provider.getTransactionCount(wallet.address);
+
+    var allPromises = Promise.all([
+        gasPricePromise,
+        balancePromise,
+        transactionCountPromise
+    ]);
+
+    var sendPromise = allPromises.then(
+        function(results) {    
+            var gasPrice         = results[0];
+            var balance          = results[1];
+            var transactionCount = results[2];
+           
+            var value = ethers.utils.parseEther(valueInETH);
+
+            var transaction = {
+                to: _addressto,
+                gasPrice: gasPrice,
+                gasLimit: ethers.utils.bigNumberify(gasLimit),
+                nonce: transactionCount,
+
+                // The amount to send
+                value: value,
+
+                // Prevent replay attacks across networks
+                chainId: provider.chainId,
+            };
+
+            var signedTransaction = wallet.sign(transaction);
+
+            // By returning a Promise, the sendPromise will resolve once the
+            // transaction is sent
+            console.log("The transaction promise was made."); 
+            console.log("The current balance is " + balance);             
+            return provider.sendTransaction(signedTransaction);
+        }
+    );
+
+    // This will be called once the transaction is sent
+    sendPromise.then(
+        function(transaction) {        
+            // This promise will be resolve once the transaction has been mined.
+            console.log("The transaction was sent: " + transaction.hash); 
+        } 
+    );
+
+    sendPromise.catch(
+        function(error) {
+            console.log("An error happened!");
+            console.log(error);
+        }
+    );
+
 }
 
-function isAddress ( _address ) {
-    if (!/^(0x)?[0-9a-f]{40}$/i.test(_address)) {
-        // check if it has the basic requirements of an address
-        return false;
-    } else if (/^(0x)?[0-9a-f]{40}$/.test(_address) || /^(0x)?[0-9A-F]{40}$/.test(_address)) {
-        // If it's all small caps or all all caps, return true
+function testParam( _param ) {
+    try {
+        ethers.utils.getAddress( _param );
         return true;
-    } else {
-        // Otherwise check each case
-        return isChecksumAddress(_address);
+    } catch(e) {
+        return false;
     }
-};
-
-function isChecksumAddress(_address) {
-    // Check each case
-    _address = _address.replace('0x','');
-    var addressHash = sha3(_address.toLowerCase());
-    for (var i = 0; i < 40; i++ ) {
-        // the nth letter should be uppercase if the nth digit of casemap is 1
-        if ((parseInt(addressHash[i], 16) > 7 && _address[i].toUpperCase() !== _address[i]) || (parseInt(addressHash[i], 16) <= 7 && _address[i].toLowerCase() !== _address[i])) {
-            return false;
-        }
-    }
-    return true;
-};
-
+}
 
 app.listen(port, () => console.log('App listening on port ' + port + '!'))
