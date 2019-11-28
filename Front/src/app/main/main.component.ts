7f8d67f1cd9d852f188da766121d8c467b1be0e0
José Nogueira <josej@bndes.gov.br>
2019-11-28 11:31:51 -0300

    adds angular to the Front

---
+++ Front/src/app/main/main.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  address: string;
+
+  constructor() { }
+
+  ngOnInit() {    
+    this.address = "0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73";
+  }
+
+  requestEth() {
+      console.log(this.address);
+      window.open('http://localhost:3000/requestEth/' + this.address , "_blank")
+  }    
+
+  generateDoc() {
+      console.log(this.address);
+      window.open('http://localhost:3000/generatepdf/' + this.address , "_blank")
+  }  
+
+}
